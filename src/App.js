99ec96476865a44b8f66ee0ccaa60a19fd57857b
HelloMandu <tjdals6695@gmail.com>
2020-10-29 20:33:40 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
 
+import MainPage from './pages/Mainpage/MainPage';
+
+import './static/index.scss'
 
 const App = () => {
   return (
     <div>
+      <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/pages/Mainpage/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/header/Header';
+
+import './MainPage.scss'
+
+const MainPage = () => {
+  return (
+    <div className="mainpage">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default MainPage;
