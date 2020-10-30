3fe413fb1cf23af6258846971ad35bfea236aadf
HelloMandu <tjdals6695@gmail.com>
2020-10-30 20:44:50 +0900

    swiper complete

--- src/components/Swiper/SwiperView.js
+++ src/components/Swiper/SwiperView.js
@@ -1,23 +1,26 @@
 import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import SwiperCore, { Autoplay, Navigation } from "swiper";
 
 import "./SwiperView.scss";
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
-// import "swiper/components/pagination/pagination.scss";
 
 const SwiperView = ({movieList}) => {
-    SwiperCore.use([Autoplay, Navigation, Pagination]);
+    SwiperCore.use([Autoplay, Navigation]);
     return (
         <Swiper
             className="swiper"
             spaceBetween={50}
             slidesPerView={1}
             centeredSlides={true}
-            pagination={{ clickable: true }}
+            navigation
             loop={true}
+            autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+            }}
         >
             {movieList.map(({ id, background_image }) => (
                 <SwiperSlide key={id} className="swiper-slide">
