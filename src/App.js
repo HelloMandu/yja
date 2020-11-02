efa5e473a107823f998053481e0331c9903b04ad
HelloMandu <tjdals6695@gmail.com>
2020-11-02 14:32:19 +0900

    merge with jh

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ import { getMovieList } from "./store/movie";
 import { getMovieDetail } from './store/detail'
 
 const App = () => {
-    // const isLoading = useSelector((state) => state.loading.isLoading);
+    const isLoading = useSelector((state) => state.loading.isLoading);
     const dispatch = useDispatch();
 
-    const detail = useSelector((state) => state.detail.detail)
+    // const detail = useSelector((state) => state.detail.detail)
 
-    // useEffect(() => {
-    //     dispatch(getMovieList('action'));
-    // }, [dispatch]);
+    useEffect(() => {
+        dispatch(getMovieList('action'));
+    }, [dispatch]);
 
-    const debug = () => {
-        dispatch(getMovieDetail(22375))
-    }
+    // const debug = () => {
+    //     dispatch(getMovieDetail(22375))
+    // }
 
     return (
         <>
-            {/* <Loading /> */}
-            {/* {!isLoading && <MainPage></MainPage>} */}
-            <div onClick={debug}>API TEST</div>
-            {console.log(detail)}
+            <Loading />
+            {!isLoading && <MainPage></MainPage>}
+            {/* <div onClick={debug}>API TEST</div>
+            {console.log(detail)} */}
             
         </>
     );
